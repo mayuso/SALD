85732827b8cc825984baae9f64a4c376b80726fd
Mayuso <mikelayuso@gmail.com>
2025-12-07 10:06:58 +0100

    Added missing include for std::find

--- src/Sald/Core/LayerStack.cpp
+++ src/Sald/Core/LayerStack.cpp
@@ -1,43 +1,33 @@
-#include "saldpch.h"
 #include "LayerStack.h"
+#include "saldpch.h"
+#include <algorithm>
 
-Sald::LayerStack::LayerStack()
-{
-    m_LayerInsert = m_Layers.begin();
+Sald::LayerStack::LayerStack() { m_LayerInsert = m_Layers.begin(); }
+
+Sald::LayerStack::~LayerStack() {
+  for (Layer *layer : m_Layers) {
+    delete layer;
+  }
 }
 
-Sald::LayerStack::~LayerStack()
-{
-    for (Layer *layer : m_Layers)
-    {
-        delete layer;
-    }
+void Sald::LayerStack::PushLayer(Layer *layer) {
+  m_Layers.emplace(m_LayerInsert, layer);
 }
 
-void Sald::LayerStack::PushLayer(Layer *layer)
-{
-    m_Layers.emplace(m_LayerInsert, layer);
+void Sald::LayerStack::PushOverlay(Layer *overlay) {
+  m_Layers.emplace_back(overlay);
 }
 
-void Sald::LayerStack::PushOverlay(Layer *overlay)
-{
-    m_Layers.emplace_back(overlay);
+void Sald::LayerStack::PopLayer(Layer *layer) {
+  auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
+  if (it != m_Layers.end()) {
+    m_Layers.erase(it);
+  }
 }
 
-void Sald::LayerStack::PopLayer(Layer *layer)
-{
-    auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
-    if (it != m_Layers.end())
-    {
-        m_Layers.erase(it);
-    }
-}
-
-void Sald::LayerStack::PopOverlay(Layer *overlay)
-{
-    auto it = std ::find(m_Layers.begin(), m_Layers.end(), overlay);
-    if (it != m_Layers.end())
-    {
-        m_Layers.erase(it);
-    }
+void Sald::LayerStack::PopOverlay(Layer *overlay) {
+  auto it = std ::find(m_Layers.begin(), m_Layers.end(), overlay);
+  if (it != m_Layers.end()) {
+    m_Layers.erase(it);
+  }
 }
